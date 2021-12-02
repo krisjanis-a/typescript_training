79cf7d797c2cb70d1455d61f9ad1af2cf3f3fa8e
krisjanis-a <krishjanis96@gmail.com>
2021-12-02 17:52:13 +0200

    first commit

---
+++ index.ts
@@ -0,0 +1,36 @@
+
+/**
+ * 
+ * @param num1
+ * @param num2
+ * @returns sum of numbers
+ */
+
+function add(num1: number, num2: number) {
+    return num1 + num2;
+}
+
+const n1 = 3;
+const n2 = 2.9;
+
+const result = add(n1,n2);
+
+console.log(result)
+
+/**
+ * 
+ * @param word1 
+ * @param word2 
+ * @returns concatenated word
+ */
+
+function concatenate (word1: string, word2: string) {
+    return word1 + word2;
+}
+
+const w1 = "ass";
+const w2 = "hat"
+
+const sentence = concatenate(w1,w2)
+
+console.log(sentence)

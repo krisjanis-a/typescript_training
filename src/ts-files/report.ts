d2d9494a531a8fb7463325fa997696bdc99f7c16
krisjanis-a <krishjanis96@gmail.com>
2021-12-03 01:27:38 +0200

    e

--- src/ts-files/report.ts
+++ src/ts-files/report.ts
@@ -1,19 +1,18 @@
+const numbers = [1, 2, 4, 8, 16, 32, 64];
+let powerOfTwo: number[];
 
-const numbers = [1,2,4,8,16,32, 64];
-let powerOfTwo:number[];
+powerOfTwo = numbers.map((num) => 2 ** num);
 
-powerOfTwo = numbers.map(num => 2**num);
-
-console.log("Reporting 2 to the power of...")
-console.log(powerOfTwo)
-
+console.log("Reporting 2 to the power of...");
+console.log(powerOfTwo);
 
 // Getting HTML DOM element
 
 const button = document.getElementById("btnFst")!;
 
-button.addEventListener("click", ()=>
-{
-    console.log("Clicked!")
-})
+function handleClick(message: string) {
+  console.log(`Dang! The message reads ${message}`);
+}
 
+if (button)
+  button.addEventListener("click", handleClick.bind(null, "Nothingness"));
